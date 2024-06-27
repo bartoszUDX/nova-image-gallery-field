d6b87cfbf029d36e5fc597666ed3b69816b834bf
bartoszUDX <bartosz.sz@udx.pl>
2024-06-27 14:39:16 +0200

    Update IndexField.vue

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="image-gallery-field">
         <div v-if="count > 0" class="relative w-8 rounded overflow-hidden h-8">
-            <img :src="thumbnail" class="object-cover w-full h-full" />
+            <light-box :index="selected" :images="images" />
+
+            <img :src="thumbnail" class="object-cover w-full h-full" @click="select(0)" />
 
             <div
                 class="bg-primary-500 text-white text-xs leading-none font-semibold absolute inline-flex items-center justify-center w-4 h-4 rounded-full right-0 bottom-0 m-1"
@@ -16,7 +18,18 @@
 <script>
 export default {
     props: ["resourceName", "field"],
+    components: {
+        LightBox,
+    },
+    data() {
+        return {
+            selected: null,
+        };
+    },
     computed: {
+        images() {
+            return Array.from(this.field.value);
+        },
         count() {
             return this.field.value && this.field.value.length
                 ? this.field.value.length
@@ -26,6 +39,11 @@ export default {
             return this.field.value && this.field.value.length
                 ? this.field.value[0].url
                 : "";
+        },
+    },
+    methods: {
+        select(index) {
+            this.selected = index;
         },
     },
 };
